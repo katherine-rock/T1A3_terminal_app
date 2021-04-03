0e10f1fe81fd16090c1b9e47a6ff521cec6ba630
katherine-rock <katherine_rock@hotmail.com>
2021-04-03 13:06:44 +1100

    Initial commit, welcome and menu

---
+++ display.rb
@@ -0,0 +1,9 @@
+def welcome
+    puts "Things To Do"
+end
+
+def menu
+    puts "Choose from the following options: "
+    options = ["Display all","Choose a category", "Add new action item", "Edit new action item", "Delete an action item", "Help", "Quit"]
+    puts options
+end
---
+++ todolist.rb
@@ -0,0 +1,5 @@
+require './display.rb'
+
+welcome
+
+menu
