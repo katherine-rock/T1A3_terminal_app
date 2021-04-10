2972ccf8f13de27fe6c004479c1110e3709af30e
katherine-rock <katherine_rock@hotmail.com>
2021-04-10 10:16:54 +1000

    Add colour coding to help screen

--- views/help.rb
+++ views/help.rb
@@ -1,15 +1,23 @@
 def help 
+    pastel = Pastel.new
     puts "  --- "
     puts "Thank you for using the 'To Do' app".blue.on_white
     puts "Below is a list of options available in the app"
     puts
-    puts "  Enter 'S' or 'show' to display a list of all current action items"
-    puts "  Enter 'C' or 'choose' to display all items within a category of your choice"
-    puts "  Enter 'A' or 'add' to add a new action item, with the option to include a category and priority"
-    puts "  Enter 'E' or 'edit' to update a previously entered action item"
-    puts "  Enter 'D' or 'delete' to delete an action item"
-    puts "  Enter 'H' or 'help' to return to this help screen"
-    puts "  Enter 'Q' or 'quit' to exit the program"
+    # Show all
+    puts "  Enter " + pastel.green("S") + " or " + pastel.green("show") + " to display a list of all current action items"
+    # Add new
+    puts "  Enter " + pastel.green("A") + " or " + pastel.green("add") + " to add a new action item, with the option to include a category and priority"
+    # Edit
+    puts "  Enter " + pastel.green("E") + " or " + pastel.green("edit") + " to update a previously entered action item"
+    # Choose category
+    puts "  Enter " + pastel.green("C") + " or " + pastel.green("choose") + " to display all items within a category of your choice"
+    # Delete
+    puts "  Enter " + pastel.green("D") + " or " + pastel.green("delete") + " to delete a previously entered action item"
+    # Help
+    puts "  Enter " + pastel.green("H") + " or " + pastel.green("help") + " to return to this help screen"
+    # Quit
+    puts "  Enter " + pastel.green("Q") + " or " + pastel.green("quit") + " to exit the program"
     puts 
     puts "For more detailed information about this app"
     puts TTY::Link.link_to("Please visit", "https://github.com/katherine-rock/T1A3_terminal_app".cyan.underline)
--- views/menu.rb
+++ views/menu.rb
@@ -2,9 +2,9 @@ def menu
     pastel = Pastel.new
     puts "Choose from the following options: "
     puts "-" + pastel.bold("Show") + " all"
-    puts "-" + pastel.bold("Choose") + " a category"
     puts "-" + pastel.bold("Add") + " a new action item"
     puts "-" + pastel.bold("Edit") + " an action item"
+    puts "-" + pastel.bold("Choose") + " a category"
     puts "-" + pastel.bold("Delete") + " an action item"
     puts "-" + pastel.bold("Help")
     puts "-" + pastel.bold("Quit")
